f79f31eb5025a51dfbf5176421e662da274283ad
tokoniewski <tokoniewski@D111-0001.pl.amx>
2016-01-21 13:31:56 +0100

    window, gui

---
+++ wingui.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   wingui.h
+ * Author: tokoniewski
+ *
+ * Created on 21 styczeń 2016, 13:15
+ */
+
+#ifndef WINGUI_H
+#define	WINGUI_H
+
+#define MUI_MENU_BARLABEL      MUI_NewObject(MUIC_Menuitem, MUIA_Menuitem_Title, NM_BARLABEL, TAG_END) 
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+Object *create_button(char *label, char *control, LONG objid);
+Object *create_menu(char *label, char *control, LONG objid);
+Object *BuildInfoWin();
+Object *BuildSearchBar();
+Object *BuildMenu()
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WINGUI_H */
+
